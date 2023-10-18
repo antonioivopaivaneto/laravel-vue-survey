289095da9ac49101d3c6a5ac16781b9762dbfc8d
junior <antonioivo.3@gmail.com>
2023-10-18 17:18:21 -0300

    add tailwind and form plugin

---
+++ laravel-vue-survey/src/store/index.js
@@ -0,0 +1,19 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+  state:{
+    user:{
+      data:{name:'Junior'},
+      token:null
+    }
+  },
+  getters:{},
+  actions:{},
+  mutations:{},
+  modules:{},
+
+});
+
+export default store;
+
+
